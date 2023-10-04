c2e12d2aa636f881f781824529f4a353e50c5e12
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-10-04 16:51:50 -0400

    updatedlecture

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const themeButton = document.createElement('button')
 
 themeButton.innerText = 'Light Mode'
 
-body.appendChild(themeButton)
+body.append(themeButton)
 
 themeButton.addEventListener('click', () => {
   if (body.style.backgroundColor === 'black') {
@@ -39,12 +39,15 @@ function darkMode() {
 }
 
 const main = document.getElementsByTagName('main')[0]
+main.style.display = 'flex'
+main.style.gap = '10px'
+
 const studentsHeader = document.createElement('h3')
 studentsHeader.innerText = 'Students'
-// main.insertBefore(studentsHeader,listContainer)
+
 
 const studentArray = ['tom garret', 'lyla', 'josh', 'porter', 'raul', 'tim']
-const studentArray2 = ['senait', 'mel', 'milad','mabel']
+const studentArray2 = ['senait', 'mel', 'milad', 'mabel']
 
 
 const listContainer = document.getElementById('student-container')
@@ -55,8 +58,8 @@ for (const [i, student] of studentArray.entries()) {
   listContainer.appendChild(li)
 }
 
-for(const [i, student] of studentArray2.entries()){
-  listContainer.innerHTML += `<li id=${i+ studentArray.length}>${titleCase(student)}</li>`
+for (const [i, student] of studentArray2.entries()) {
+  listContainer.innerHTML += `<li id=${i + studentArray.length}>${titleCase(student)}</li>`
 }
 
 function titleCase(astring) {
@@ -71,38 +74,40 @@ function titleCase(astring) {
 const pokeForm = document.querySelector('#poke-form')
 
 pokeForm.addEventListener('submit', async (e) => {
-    e.preventDefault()
-    const pokemonName = getPokeName()
-    console.log(pokemonName)
-    const pokeData = await pokeApiCall(pokemonName)
-    makePokeCard(pokeData)
+  e.preventDefault()
+  const pokemonName = getPokeName()
+  console.log(pokemonName)
+  const pokeData = await pokeApiCall(pokemonName)
+  makePokeCard(pokeData)
 })
 
 function getPokeName() {
-    const nameInput = document.querySelector('#poke-field').value
+  return document.querySelector('#poke-field').value
 }
 
-async function PokeApiCall() {
-//     https://pokeapi.co/api/v2/pokemon/<pokemon>
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemonName}')
-    if(res.ok){
-        const data = res.json()
-        console.log(data.versions['generation-v']['black-white'].animated.front_default ??
-        return {
-            name: data.name,
-            weight: data.weight,
-            abilities: getAbilityNames(data.abilities)
-        }
-        console.log(output)
-    } else window.alert('Invalid Pokename')
+async function pokeApiCall(pokemonName) {
+  // https://pokeapi.co/api/v2/pokemon/<pokemon>
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+  if (res.ok) {
+    const data = await res.json()
+    return {
+      name: data.name,
+      weight: data.weight,
+      abilites: getAbilityNames(data.abilities),
+      imgUrl: data.sprites.versions['generation-v']['black-white'].animated.front_default ?? data.sprites.front_default
+    }
+  } else window.alert('Invalid Pokename')
 }
 
-function getAbilityNames(abilities){
-  const abilityNameArray = abilities.map( ob => ob.ability.name)
+function getAbilityNames(abilites) {
+  const abilityNameArray = abilites.map((ob) => ob.ability.name)
   return abilityNameArray
 }
 
-function makePokeCard({abilities, imgUrl, weight, name }) {
+const pokeCardContainer = document.querySelector('.poke-card-container')
+pokeCardContainer.style.display = 'flex'
+
+function makePokeCard({abilites, imgUrl, weight, name}) {
 
   const pokeCard = document.createElement('div')
   const pokeHeading = document.createElement('h3')
@@ -110,21 +115,21 @@ function makePokeCard({abilities, imgUrl, weight, name }) {
   const attributeContainer = document.createElement('ul')
   const pokeImg = document.createElement('img')
 
-for (const abilityName of abilities) {
-    
-  pokeHeading.innerText = titleCase(pokeData.name)
-  weightPTag.innerText = pokeData.weight
-  pokeCard.append(pokeHeading, weightPTag, attributeContainer)
-  body.appendChild(pokeCard)
-}
+  for (const abilityName of abilites) {
+    const abilityListItem = document.createElement('li')
+    abilityListItem.innerText = abilityName
+    attributeContainer.appendChild(abilityListItem)
+  }
 
-pokeImg.src = imgUrl
-pokeHeading.innerText = titleCase(name)
-weightPTag.innerText = 'Weight' + weight
-pokeCard.append(pokeHeading, weightPTag, pokeImg, attributeContainer) 
-// for(const abilityName of pokeData.abilities){
-//   const abilityListItem = document.createElement('li')
-//   abilityListItem.innerText = abilityName
-//   attributeContainer.appendChild(abilityListItem)
-// }
-pokeCard.addEventListener('click')
+  pokeImg.src = imgUrl
+  pokeHeading.innerText = titleCase(name)
+  weightPTag.innerText = 'Weight ' + weight
+  pokeCard.append(pokeHeading, weightPTag, pokeImg, attributeContainer)
+  // document.getElementsByClassName('poke-card-container')[0].appendChild(pokeCard)
+  
+  pokeCardContainer.appendChild(pokeCard)
+
+  pokeCard.addEventListener('click', () => {
+    pokeCard.remove()
+  })
+}
